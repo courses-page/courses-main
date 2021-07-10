030b93d94ffa3feb581c0ad49860894f01ade141
Marcos Salvo <marcossalvo95@gmail.com>
2021-07-10 14:01:45 +0200

    User and company models created

--- models/Company.model.js
+++ models/Company.model.js
@@ -3,11 +3,11 @@ const { Schema, model } = require("mongoose");
 const emailRegex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i; 
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema({
-  username: {
+const companySchema = new Schema({
+  companyName: {
     type: String,
-    required: [true, "Username is required"],
-    minlegth: [3, "Username should be at least 3 chars long"]
+    required: [true, "Company name is required"],
+    minlegth: [3, "Company name should be at least 3 chars long"]
   },
   email: {
     type: String,
@@ -22,15 +22,16 @@ const userSchema = new Schema({
     required: [true, "Password is required"],
     minlegth: [8, "Password should be at least 8 chars long"]
   },
-  fullName: {
-    type: String
+  description: {
+    type: String,
+    minlegth: [25, "Description should be at least 25 chars long"]
   },
   imageUrl: {
     type: String,
-    default: "https://www.stevensegallery.com/640/360"
+    default: "https://picsum.photos/640/360"
   }
 });
 
-const User = model("User", userSchema);
+const Company = model("Company", companySchema);
 
-module.exports = User;
+module.exports = Company;
