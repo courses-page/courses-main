36495e555dfe8fbd08d87b7ae8b04b15b025f8b2
Marcos Salvo <marcossalvo95@gmail.com>
2021-07-28 20:33:08 +0200

    Web finished V.1

--- public/js/myProfile.js
+++ public/js/myProfile.js
@@ -5,10 +5,19 @@ document.addEventListener(
   
       const changeProfilePicture = document.getElementById("changeProfilePicture");
       const pictureForm = document.getElementById("pictureForm");
+
+      // const toggleStyle = (el) => {
+      //   if(el.style.display == "none") {
+      //     el.style.justifyContent = ""
+      //   } else {
+      //     el.style.justifyContent = "center"
+      //   }
+      // }
   
       changeProfilePicture.addEventListener("click", () => { 
         pictureForm.classList.toggle("visible");
         pictureForm.classList.toggle("hidden");
+        // toggleStyle(pictureForm)
       })
   
     },
